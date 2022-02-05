ff2fcc6549c9d637dc40315935c60fd9fd04327a
glebcher <gleb4er@gmail.com>
2022-02-05 10:17:50 +0300

    init

---
+++ Зачетное задание/Зачетное задание/DoubleLinkedList/node.py
@@ -0,0 +1,29 @@
+from typing import Any, Optional
+
+
+class Node:
+    def __init__(self, value: Any, next_: Optional["Node"] = None):
+        self.value = value
+        self.next = next_
+
+    def __repr__(self) -> str:
+        return f"Node({self.value}, {None})" if self.next is None else f"Node({self.value}, Node({self.next}))"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    def is_valid(self, node: Any) -> None:
+        if not isinstance(node, (type(None), Node)):
+            raise TypeError
+
+    def setNext(self, next_node):
+        self.next = next_node
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, next_: Optional["Node"]):
+        self.is_valid(next_)
+        self._next = next_
